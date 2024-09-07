0479ef5cac0f1f92fe79de0f13b3be6e62f61d6c
Gokul Krishna jayan <91828907+Gokulkrishnajayan@users.noreply.github.com>
2024-09-07 12:42:28 +0000

    false

--- js/main.js
+++ js/main.js
@@ -59,11 +59,12 @@
             $('.back-to-top').fadeIn('slow');
         } else {
             $('.back-to-top').fadeOut('slow');
+            return false;
         }
     });
     $('.back-to-top').click(function () {
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return;
+        return ;
     });
 
     // Facts counter
