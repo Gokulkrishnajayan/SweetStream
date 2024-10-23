af42ab965e70e3583646fe06f4a834fd306c49f7
gokulkrishnajayan <gokuljayan508@gmail.com>
2024-10-24 01:27:27 +0530

    session user

--- session/session_user.php
+++ session/session_user.php
@@ -2,5 +2,4 @@
 include $_SERVER['DOCUMENT_ROOT'] .'/SweetStream/session/session_check.php'; // Include session validation
 checkPrivilege('user'); // Ensure only users can access
 
-echo "Welcome, User " . $_SESSION['user_id'] . "!";
 ?>
