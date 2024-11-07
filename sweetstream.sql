16d0fd2daf90cae2b2bdc9194c6eb15253db3a39
gokulkrishnajayan <gokuljayan508@gmail.com>
2024-11-08 00:22:23 +0530

    "Updated database schema: modified delivery_table, product_table, and user_table structures; added new data to delivery_table, product_table, and user_table; updated auto_increment values."

--- sweetstream.sql
+++ sweetstream.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 28, 2024 at 07:36 PM
+-- Generation Time: Nov 07, 2024 at 07:41 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
 
@@ -56,28 +56,35 @@ CREATE TABLE `delivery_table` (
   `price` varchar(100) NOT NULL,
   `address` varchar(100) NOT NULL,
   `phone_no` varchar(10) NOT NULL,
-  `deliveryperson_id` int(100) NOT NULL,
+  `deliveryperson_id` varchar(100) NOT NULL,
   `delivery_date_time` datetime NOT NULL DEFAULT current_timestamp(),
   `status` varchar(100) NOT NULL,
-  `payment_details` varchar(100) NOT NULL
+  `payment_details` varchar(100) NOT NULL,
+  `payment_status` varchar(100) NOT NULL,
+  `delivery_dispacted_time` datetime DEFAULT NULL,
+  `delivery_delivered_time` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `delivery_table`
 --
 
-INSERT INTO `delivery_table` (`did`, `product_id`, `user_id`, `product_quantity`, `price`, `address`, `phone_no`, `deliveryperson_id`, `delivery_date_time`, `status`, `payment_details`) VALUES
-(7, 59, '17', '2', '240', 'njallikkatile', '7994506504', 0, '2024-10-26 20:23:14', 'pending', 'Cash on Delivery selected.'),
-(8, 58, '17', '3', '123', 'njallikkatile', '7994506504', 0, '2024-10-26 20:23:14', 'pending', 'Cash on Delivery selected.'),
-(9, 59, '17', '5', '240', 'njallikkatile', '7994506504', 0, '2024-10-26 20:24:40', 'pending', 'Paid using Net Banking.'),
-(10, 59, '17', '4', '240', 'njallikkatile', '7994506504', 0, '2024-10-26 20:32:41', 'pending', 'Cash on Delivery selected.'),
-(11, 59, '17', '10', '240', 'njallikkatile', '7994506504', 0, '2024-10-26 20:40:33', 'pending', 'Cash on Delivery selected.'),
-(12, 58, '17', '101', '123', 'njallikkatile', '7994506504', 0, '2024-10-26 22:24:15', 'pending', 'Cash on Delivery selected.'),
-(13, 59, '17', '10', '240', 'njallikkatile', '7994506504', 0, '2024-10-26 22:25:10', 'pending', 'Cash on Delivery selected.'),
-(14, 59, '11', '2', '240', 'njallikkatile', '7994506504', 0, '2024-10-27 19:21:13', 'pending', 'Cash on Delivery selected.'),
-(15, 59, '11', '1', '240', 'njallikkatile', '7994506505', 0, '2024-10-27 19:57:48', 'pending', 'Paid using Net Banking.'),
-(16, 59, '11', '1', '240', 'njallikkatile', '7994506505', 0, '2024-10-27 20:09:40', 'pending', 'Cash on Delivery selected.'),
-(17, 59, '11', '2', '240', 'njallikkatile', '7994506505', 0, '2024-10-28 19:27:27', 'pending', 'Cash on Delivery selected.');
+INSERT INTO `delivery_table` (`did`, `product_id`, `user_id`, `product_quantity`, `price`, `address`, `phone_no`, `deliveryperson_id`, `delivery_date_time`, `status`, `payment_details`, `payment_status`, `delivery_dispacted_time`, `delivery_delivered_time`) VALUES
+(23, 62, '11', '1', '123', 'njallikkatt', '7994506507', '11', '2024-11-02 12:04:39', 'Order Dispatched', 'Cash on Delivery.', 'paid', '2024-11-06 18:51:50', NULL),
+(24, 63, '11', '1', '133', 'njallikkatt', '7994506507', '11', '2024-11-02 12:04:39', 'Order Dispatched', 'Cash on Delivery.', '', '2024-11-06 18:51:50', NULL),
+(25, 64, '11', '1', '234', 'njallikkatt', '7994506507', '11', '2024-11-02 12:04:39', 'Order Dispatched', 'Cash on Delivery.', '', '2024-11-06 18:51:50', NULL),
+(26, 63, '11', '1', '133', 'njallikkatt', '7994506507', '11', '2024-11-06 10:28:58', 'Order Dispatched', 'Cash on Delivery.', '', '2024-11-07 00:28:53', NULL),
+(27, 62, '11', '2', '123', 'njallikkatt', '7994506507', '11', '2024-11-06 10:28:58', 'Order Dispatched', 'Cash on Delivery.', '', '2024-11-07 00:28:53', NULL),
+(28, 63, '11', '1', '133', 'njallikkatt', '7994506507', '11', '2024-11-07 12:33:07', 'Order Dispatched', 'Paid using Net Banking.', '', '2024-11-07 00:33:19', NULL),
+(29, 64, '11', '1', '234', 'njallikkatt', '7994506507', '11', '2024-11-07 12:39:24', 'Order Dispatched', 'Cash on Delivery.', '', '2024-11-07 00:39:55', NULL),
+(30, 64, '11', '1', '234', 'njallikkatt', '7994506507', '11', '2024-11-07 12:46:35', 'Order Dispatched', 'Cash on Delivery.', '', '2024-11-07 00:46:43', NULL),
+(31, 63, '11', '1', '133', 'njallikkatt', '7994506507', '11', '2024-11-07 12:48:30', 'Order Dispatched', 'Cash on Delivery.', '', '2024-11-07 00:48:40', NULL),
+(32, 64, '11', '1', '234', 'njallikkatt', '7994506507', '11', '2024-11-07 12:50:13', 'Order Dispatched', 'Paid using UPI.', '', '2024-11-07 00:50:20', NULL),
+(33, 63, '18', '1', '133', 'njallikkatile', '7994506507', '7', '2024-11-07 10:45:02', 'Order Dispatched', 'Cash on Delivery.', '', '2024-11-07 22:46:08', NULL),
+(34, 64, '18', '1', '234', 'njallikkatile', '7994506507', '7', '2024-11-07 11:17:29', 'Order Dispatched', 'Cash on Delivery.', '', '2024-11-07 23:17:48', NULL),
+(35, 62, '18', '2', '123', 'njallikkatile', '7994506507', '7', '2024-11-07 11:34:53', 'Order Dispatched', 'Paid using Net Banking.', 'paid', '2024-11-07 23:35:25', NULL),
+(36, 63, '18', '2', '133', 'njallikkatile', '7994506507', '7', '2024-11-07 11:34:53', 'Order Dispatched', 'Paid using Net Banking.', '', '2024-11-07 23:35:25', NULL),
+(37, 63, '19', '1', '133', 'njallikkatile', '7994506507', '7', '2024-11-07 11:47:51', 'Order Dispatched', 'Cash on Delivery.', 'not paid', '2024-11-07 23:48:07', NULL);
 
 -- --------------------------------------------------------
 
@@ -99,8 +106,9 @@ CREATE TABLE `product_table` (
 --
 
 INSERT INTO `product_table` (`pid`, `pname`, `pphoto`, `pdescription`, `pprice`, `current_stock`) VALUES
-(58, 'payasam aluva', '/SweetStream/product/img_671933f12f04f6.74765097.jpg', '123', '123', ''),
-(59, 'parippu', '/SweetStream/product/img_671936a3481dd3.10390817.jpg', 'parippu payasam', '240', '');
+(62, 'payasam aluva', '/SweetStream/product/img_67251a96bf1905.16554596.jpg', '123', '123', ''),
+(63, 'payasam', '/SweetStream/product/img_67251aa64b3d85.19405276.jpg', '123', '133', ''),
+(64, 'parippu', '/SweetStream/product/img_67251ab5537b96.72306276.jpg', '31', '234', '');
 
 -- --------------------------------------------------------
 
@@ -125,8 +133,9 @@ CREATE TABLE `user_table` (
 INSERT INTO `user_table` (`id`, `name`, `phone_no`, `email`, `password`, `address`, `privilege`) VALUES
 (3, 'Gokul krishna jayan', '121221', 'gokuljayan508@gmail.com', '$2y$10$n4sp3Cm9YvWZ4CxKrzyfuu6IY3.FdqrV1mgdr4zfB8FZyJB9Uaxyu', 'njallikkattile house', 'admin'),
 (7, 'ginse', '0', 'ginse@gmail.com', '$2y$10$Cj4bKj1ZgYhooryg3i1LFu1jn3JnVxkKfUGlbbXtzNxxWYB2BAYUe', 'swdf', 'delivery'),
-(11, 'tony', '7994506505', 'tony@gmail.com', '$2y$10$IRkosU.9PC9MjqO9XQGIh.kaB.MAGumR4Ov2o6paJkCW6C8NJ0h42', 'njallikkatile', 'user'),
-(17, 'dragon', '7994506504', 'dragon@gmail.com', '$2y$10$caR95jFsvpYo1gy7j/Vh.uG62mau5RW3pWXmrsFRvQgZ1UVUuFpxK', 'njallikkatile', 'user');
+(11, 'tony', '7994506507', 'tony@gmail.com', '$2y$10$IRkosU.9PC9MjqO9XQGIh.kaB.MAGumR4Ov2o6paJkCW6C8NJ0h42', 'njallikkatt', 'user'),
+(18, 'blesson', '7994506507', 'blesson@gmail.com', '$2y$10$v3qsYjC0gGx5l/XloHJHJ.JIMrk4MIhvDZoZawer8295hVeswG0x6', 'njallikkatile', 'user'),
+(19, 'dragon', '7994506507', 'dragon@gmail.com', '$2y$10$NC1po2YZl0dx07n6An.BFu1W2/xO4GSfhy1D7JBjhPz.ED/hsFcnu', 'njallikkatile', 'user');
 
 --
 -- Indexes for dumped tables
@@ -165,25 +174,25 @@ ALTER TABLE `user_table`
 -- AUTO_INCREMENT for table `cart_table`
 --
 ALTER TABLE `cart_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
 
 --
 -- AUTO_INCREMENT for table `delivery_table`
 --
 ALTER TABLE `delivery_table`
-  MODIFY `did` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `did` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `product_table`
 --
 ALTER TABLE `product_table`
-  MODIFY `pid` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
+  MODIFY `pid` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
 --
 -- AUTO_INCREMENT for table `user_table`
 --
 ALTER TABLE `user_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
