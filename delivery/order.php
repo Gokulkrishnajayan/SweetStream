9579aa50ce18b5ff32a604a16f3410bcdde7faba
Gokul Krishna jayan <91828907+Gokulkrishnajayan@users.noreply.github.com>
2024-11-17 15:21:51 +0000

    add feature one by one

--- delivery/order.php
+++ delivery/order.php
@@ -11,7 +11,7 @@ $sql = "
         u.name AS customer_name, u.phone_no AS customer_phone
     FROM delivery_table d
     JOIN user_table u ON d.user_id = u.id
-    WHERE (d.status = 'pending' OR d.deliveryperson_id IS NULL)
+    WHERE (d.status = 'pending' OR d.deliveryperson_id IS NULL OR d.status = 'Assigned' )
 ";
 
 // If search term exists, filter the query based on the search input
